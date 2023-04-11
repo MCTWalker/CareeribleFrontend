f379096baeeecf393801a1940cff43765ab1878c
Maura Walker <maura.walker48@gmail.com>
2023-04-11 12:49:56 -0500

    adding careerible image

--- src/App.js
+++ src/App.js
@@ -8,18 +8,10 @@ function App() {
     <div className="App">
       <PageHeader></PageHeader>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"images/image0.jpeg"} className="App-logo" alt="logo" />
         <p>
           Careerible.com, making you more <br/> <MarketingFade></MarketingFade>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
