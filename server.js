a483c5c3d6935d969a21aa167d65813f05427782
Maura Walker <maura.walker48@gmail.com>
2023-04-26 11:43:25 -0500

    making server that serves build

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const containingFolder = "build";
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.resolve(containingFolder, 'index.html'));
 })
+
+app.get('/static/*', (req, res) =>  {
+  res.sendFile(path.resolve("./" + containingFolder + req.originalUrl));
+});
+
+app.get('/static/*', (req, res) =>  {
+  res.sendFile(path.resolve("./" + containingFolder + req.originalUrl));
+});
+
+app.get('/images/*', (req, res) =>  { 
+  res.sendFile(path.resolve("./" + containingFolder + req.originalUrl));
+});
+
+app.get('*', (req, res) => res.sendFile(path.resolve(containingFolder, 'index.html')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
